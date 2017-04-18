cea5a8ccba84d6ed3e94f8ac1baabe4dc25e14a6
Alfredo Oltra <alfredo.ptcf@gmail.com>
2017-04-18 03:13:21 +0200

    Entrada por parámetros

--- SW/Core/Filtros/MC-C1/MCC1.py
+++ SW/Core/Filtros/MC-C1/MCC1.py
@@ -11,13 +11,21 @@
 Ejecuta el filtro 1 (MCC1): relevante/no relevante
 """
 
+import sys
 from sklearn.externals import joblib
 # pylint: disable=unused-import
 from LimpiarTextoTransf import LimpiarTextoTransf
 
+if len(sys.argv) != 2:
+    print("No existe una definición clara del texto a procesar. Tal vez falten comillas")
+    sys.exit()
+
+texto = []
+texto.append(sys.argv[1])
+
 PIPELINE = joblib.load('MCC1.mym')
 
-PREDICCION = PIPELINE.predict(["¡Hola, buenos días!"])
+PREDICCION = PIPELINE.predict(texto)
 
 if PREDICCION[0] == "0":
     print('No relevante')
